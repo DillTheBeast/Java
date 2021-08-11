fec8f11b762adfd0bad0d71d671e90e5cdaefee2
DillTheBeast <68816809+DillTheBeast@users.noreply.github.com>
2021-08-11 13:26:39 -0400

    Learning classes and objects

--- Person.java
+++ Person.java
@@ -1,9 +1,58 @@
 public class Person {
+    private String eyecolor;
+    private String haircolor;
+    private String skintone;
+    private String gender;
+    private int age;
+    private boolean alive = true;
     public static void main(String[] args) {
-    
+        
+    }
+
+    public Person(String eyecolor, String haircolor, String skintone, String gender, int age) {
+        this.eyecolor = eyecolor;
+        this.haircolor = haircolor;
+        this.skintone = skintone;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public String getHairColor() {
+        return haircolor;
+    }
+
+    public void dyeHair(String haircolor) {
+        this.haircolor = haircolor;
+    }
+        
+    public String getEyeColor() {
+        return eyecolor;
+    }
+
+    public String getSkintone() {
+        return skintone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void changeGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
     }
     
-    public Person() {
+    public void Birthday(int age) {
+        this.age = age;
+    }
 
+    public boolean getAlive() {
+        return alive;
+    }
+    public void Dead(boolean alive) {
+        this.alive = alive;
     }
 }
