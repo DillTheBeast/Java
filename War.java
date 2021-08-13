b695d2a0eab7063807735d20057e3e63f23fbf9c
DillTheBeast <68816809+DillTheBeast@users.noreply.github.com>
2021-08-13 11:22:05 -0400

    War is done

--- War.java
+++ War.java
@@ -1,3 +1,5 @@
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 public class War {
     public static String input;
@@ -13,19 +15,18 @@ public class War {
         for(int y = 0; y < 26; y++) {
             d2.add(d.draw());
         }
-        //System.out.println("Welcome to War. Type in d to draw your top card.");
+        System.out.println("Welcome to War. Type in d to draw your top card.");
         while(Winner == false) {
-        //input = Scan.nextLine();
-        //validinput();
-        //System.out.println(d);
-        //Finding out the computers card vs the players card
+        input = Scan.nextLine();
+        validinput();
+//Finding out the computers card vs the players card
             Card a = d.draw();
             System.out.println("You drew " + a);
             Card b = d2.draw();
             System.out.println("Your opponent drew " + b);
             win(a, b, d, d2);
             winwhole(d, d2);
-            //System.out.println("Type d to draw your next card.");
+            System.out.println("Type d to draw your next card.");
         }
     }
 //Method checks for the winner of the entire game
@@ -34,42 +35,68 @@ public class War {
         if(d.size == 0 || d2.size == 0) {
 //If there is winner then find out if the player lost
             if(d.size == 0) {
-                System.out.println("You lose. RIP. Would you like to play again?");   
+                System.out.println("You lose. RIP. Thanks for playing. See you later."); 
+                System.exit(0);  
             }
 //If there is winner then find out if the player won
             if(d2.size == 0) {
-                System.out.println("You win! Nice Job! Would you like to play again?");
-            }
-//Finding out if they want to play again
-            winput = Scan.nextLine();
-            if(winput.toLowerCase() == "yes") {
-                System.out.println("Ok then lets go again");
-            }
-            else {
-                System.out.println("Ok thanks for playing. See you later.");
-                Winner = true;
+                System.out.println("You win! Nice Job! Thanks for playing. See you later.");
+                System.exit(0); 
             }
         }
     }
 //Method checks for the winner of this round
-    public static void win(Card a, Card b, Deck d, Deck d2) {
+    public static int win(Card a, Card b, Deck d, Deck d2) {
 //Lose
         if(a.getNumber() < b.getNumber()) {
             System.out.println("You lost this match. RIP :(");
             d2.add(a);
             d2.add(b);
+            return 0;
         }
 //Win
         if(a.getNumber() > b.getNumber()) {
             System.out.println("You won this match. Nice!");
             d.add(a);
             d.add(b);
+            return 1;
         }
 //Tie
         if(a.getNumber() == b.getNumber()) {
-            System.out.println("You have tied. Type d to go into war.");
-            win(a, b, d, d2);
+            System.out.println("You have tied. You are going into war");
+            if(d.CardList.size() < 4 || d2.CardList.size() < 4) {
+                if(d.CardList.size() < 4) {
+                    System.out.println("You lose. RIP. Would you like to play again?");
+                     
+                }
+                if(d2.CardList.size() < 4) {
+                    System.out.println("You win! Nice Job! Would you like to play again?");
+                    
+                }
+                winput = Scan.nextLine();
+            if(winput.toLowerCase().contains("y") ) {
+                System.out.println("Ok then lets go again");
+            }
+            else {
+                System.out.println("Ok thanks for playing. See you later.");
+                System.exit(0);
+            }
+            }
+            Card[] wager = {d.draw(), d.draw(), d.draw(), d2.draw(), d2.draw(), d2.draw()};
+            if (win(d.draw(), d2.draw(), d, d2)==1){
+                for (Card i:wager){
+                    d.add(i);
+                }
+                return 1;
+            }
+            else if(win(d.draw(), d2.draw(), d, d2)==1){
+                for (Card i:wager){
+                    d2.add(i);
+                }
+                return 0;
+            }
         }
+        return -1;
     }
 //Method makes sure they type d to draw there card
 //If not then ask them to type d again
