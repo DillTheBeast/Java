818ed216756303f4833e30f2f040ee3d5e0557c9
DillTheBeast <dillonmaltese@gmail.com>
2022-07-12 18:16:15 -0400

    Upload

---
+++ Hangman.java
@@ -0,0 +1,60 @@
+//Importing the needed materials
+import java.util.Scanner;
+
+class Hangman {
+  public static void main(String[] args) {
+//Declaring variables
+    Scanner Scan = new Scanner(System.in);
+    String word = "hello";
+    int ogLength = word.length();
+    String ogWord = word;
+    int x = 0;
+    int l = 0;
+    int w = 0;
+    String[] holder = new String[ogWord.length()];
+    int idx;
+//Introducing the game and making the first board
+    System.out.println("Let's play a game of hangman. I am thinking of a word and you have to guess the letters. If you guess a wrong letter than you get and X. If you get 5 X's then you lose.");
+    
+    for(int h = 0; h < holder.length; h++) {
+      holder[h] = "_ ";
+    }
+    System.out.println("");
+    System.out.println("Here is the board. The word is a 4 letter word.");
+    
+    while(x < 5) {
+      for(int g = 0; g < holder.length; g++) {
+        System.out.print(holder[g]);
+      }
+      System.out.println("");
+      System.out.println("Guess any letter that you would like");
+      String input = Scan.nextLine();
+      idx = word.indexOf(input);
+
+      if(idx == -1|| input == " ") {
+        System.out.println("Wrong guess. That is one X for you.");
+        x++;
+        l++;
+        System.out.println("Your X counter is at " + x);
+      }
+      else if(idx >= 0) {
+        System.out.println("You got a letter right nice.");
+        holder[idx] = input + " ";
+        word = word.substring(0, idx) + " " + word.substring(idx + 1);
+        idx = word.indexOf(input);
+        w++;
+      }
+      if(w == ogLength) {
+        System.out.println("You won. Nice Job. The word was " + ogWord);
+        break;
+      }
+    }
+    if(l == 5) {
+    System.out.println("You lost. L + RATIO Bozo.");
+    }
+
+
+    
+
+  }
+}
---
+++ SubTest.java
@@ -0,0 +1,27 @@
+import javax.sound.sampled.SourceDataLine;
+
+class SubTest {
+    public static void main(String[] args){
+        //String firstHalf = word.substring(0, 2);
+        //String secondHalf = word.substring(2);
+        String word = "Hello";
+        String ogWord = word;
+
+        System.out.println(ogWord);
+
+        word = "bird";
+        System.out.println(ogWord);
+
+        //Before update
+        //System.out.println(word);
+
+        
+        //word = word.substring(0, 2) + word.substring(3);
+
+        // System.out.println("First Half: " + firstHalf);
+        // System.out.println("Second Half: " + secondHalf);
+
+        //After update
+        //System.out.println("Deleted letter: " + word);
+    }
+}
