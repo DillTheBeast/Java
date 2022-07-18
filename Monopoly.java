6cd0243b358a6a363189bff67bde3a83bfca74b2
DillTheBeast <dillonmaltese@gmail.com>
2022-07-18 10:17:25 -0400

    Update Monopoly.java

--- Monopoly.java
+++ Monopoly.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Monopoly {
     public static void main(String[] args) {
-        
+        //Declaring variables
+        Scanner Scan = new Scanner(System.in);
     }
 }
