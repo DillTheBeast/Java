3e0e78293f1d9fb1ae92a7c80dcf5d38050b2de8
DillTheBeast <dillonmaltese@gmail.com>
2022-07-19 11:18:01 -0400

    Update Encryption.java

--- Encryption.java
+++ Encryption.java
@@ -5,9 +5,9 @@ public class Encryption {
         System.out.println("Choose a word or message for me to encrypt.");
         String input = Scan.nextLine();
         System.out.println("Choose a key.");
-        int input2 = Scan.nextInt();
+        int key = Scan.nextInt();
         int idx = 0;
-        System.out.println(Ceaser(input, idx, input2));
+        System.out.println(railFence(input, key, idx));
         //Alphabet encryption and decoder
         /*
         System.out.println(alphabet(input, idx));
@@ -36,24 +36,35 @@ public class Encryption {
         return "";
     }
     */
-    public static String Ceaser(String input, int idx, int input2) {
+    /*
+    public static String Ceaser(String input, int idx, int key) {
         int arr[] = new int [input.length()];
         char convert[] = new char[arr.length];
         String newWord = "";
             for(int a = 0; a < input.length(); a++) {
-                arr[a] = input.charAt(idx) + input2;
+                arr[a] = (input.charAt(idx) + key); 
+                arr[a] = ((arr[a] - 64) + key) % 26 + 64;
                 idx++;
             }
             for(int b = 0; b < input.length(); b++) {
                 convert[b] = (char)(arr[b]);
                 newWord = String.valueOf(convert);
-                
             }
-
         System.out.println(newWord);
         return"";
     }
-    public static String railFence() {
+    */
+    public static String railFence(String input, int key, int idx) {
+        char arr[] = new char [input.length()];
+        int index = input.indexOf(key+1);
+        System.out.println(index);
+        /*
+        for(int a = 0; a < input.length(); a+=key+1) {
+            //Need to have letter 1 skip the next however many the key is and take the number after that
+            //arr[a] = index;
+            System.out.println(arr[a]);
+        }
+        */
         return "";
     }
     public static String Beufort() {
