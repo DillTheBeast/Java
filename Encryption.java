3a02035f09bd5e1690d23becd8263a14845ea692
DillTheBeast <dillonmaltese@gmail.com>
2022-07-21 11:17:11 -0400

    d

--- Encryption.java
+++ Encryption.java
@@ -2,13 +2,19 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class Encryption {
     public static void main(String[] args) {
+        int idx = 0;
         Scanner Scan = new Scanner(System.in);
         System.out.println("Choose a word or message for me to encrypt.");
         String input = Scan.nextLine();
+        //Only used for railFence
+        /*
         System.out.println("Choose a key.");
         int key = Scan.nextInt();
-        int idx = 0;
-        System.out.println(railFence(input, key, idx));
+        */
+        //Only for Beufort
+        System.out.println("Choose a keyword to help cypher your word");
+        String bKey = Scan.nextLine();
+        System.out.println(Beufort(input, bKey, idx));
         //Alphabet encryption and decoder
         /*
         System.out.println(alphabet(input, idx));
@@ -55,8 +61,13 @@ public class Encryption {
         return"";
     }
     */
+    /*
     public static String railFence(String input, int key, int idx) {
         ArrayList<ArrayList<Character>> al = new ArrayList<ArrayList<Character>>(key); 
+        int position;
+        char letter;
+        int n = 0;
+        boolean isGoingUp = true;
 
         for(int  i = 0; i < key; i++){
             al.add(new ArrayList<Character>());
@@ -64,20 +75,55 @@ public class Encryption {
 
 
         for(int i = 0; i < input.length(); i++) {
-            char letter = input.charAt(i);
-            int position = i % key;
+            letter = input.charAt(i);
+            position = n % key;
             al.get(position).add(letter);
+            System.out.println(n);
+
+            if(isGoingUp == false) {
+                n--;
+                System.out.println("Test1");
+            }
+
+            if(isGoingUp == true) {
+                n++;
+                System.out.println("Test2");
+            }
+            if(n == key-1) {
+                isGoingUp = false;
+                System.out.println("Test3");
+            }
+
+            if(n == 0) {
+                isGoingUp = true;
+                System.out.println("Test4");
+            }
+            
+            
+            
+            
+
             //System.out.println(al.get(position).toString());
         }
         for(int i = 0; i < key; i++) {
             al.addAll(al);
             int finalposition = i % key;
-            System.out.print(al.get(finalposition).toString() + " ");
+            System.out.print(al.get(finalposition) + " ");
         }
 
         return "";
     }
-    public static String Beufort() {
-        return"";
+    public static void print(ArrayList<ArrayList<Character>> al) {
+        
+    }
+    */
+    public static String Beufort(String input, String bKey, int idx) {
+        int idx2 = idx;
+        char keyStream[] = new char[input.length()];
+        for(int i = 0; i < input.length(); i++) {
+            keyStream[i] = bKey.charAt(idx);
+            System.out.println(keyStream[i]);
+        }
+        return "";
     }
 }
