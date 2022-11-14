d807e2dfd830bcc9c17a943a686ae24d9280ef9c
DillTheBeast <dillonmaltese@gmail.com>
2022-11-14 10:53:29 -0500

    t

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,20 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-
-        int number = 13;
-        String name = "Dillon";
-        String state = "Florida";
-        boolean answer = true;
-
-
-        System.out.println ( "I am " + number);
-        System.out.println ("My name is " + name);
-        System.out.println ("I am 13 going into 8th grade" + " My Birthday is in November");
-        System.out.println ("My favorite sport is Baseball");
-        System.out.println ("My favorite state is " + state);
-        System.out.println ("As of right now my favorite game is Valorant");
-        System.out.println ("Do I like Brocoli?");
-        System.out.println (answer);
+        System.out.println("Hello World");
     }
 
 }
