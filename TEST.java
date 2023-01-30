c10f62664e670b9e9b68167a18714c6c7264ba09
DillTheBeast <68816809+DillTheBeast@users.noreply.github.com>
2023-01-30 17:08:36 -0500

    t

---
+++ TEST.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class TEST {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scan.nextInt();
+        System.out.println("Your age is " + age);
+    }
+}
