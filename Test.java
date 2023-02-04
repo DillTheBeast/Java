cad0e6175f2ba4951693f22a7fda745dd3a8767e
DillTheBeast <dillonmaltese@gmail.com>
2023-02-04 17:53:41 -0500

    t
    t

---
+++ Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your age");
+        String age = scan.nextLine();
+
+        if(age.equals"fourteen") {
+            System.out.println("This is Kaneal's age");
+        }
+
+    }
+}
