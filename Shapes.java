1adf5edd45b40058dcbbdb1dc590dccb9434b185
DillTheBeast <68816809+DillTheBeast@users.noreply.github.com>
2023-02-20 14:04:49 -0500

    Update Shapes.java

--- Shapes.java
+++ Shapes.java
@@ -3,145 +3,163 @@ import java.util.Scanner;
 public class Shapes {
     public static void main(String[] args) {
     
-//  Square
-/*
-    for (int y = 0; y < 5; y++) {
-        for(int x = 0; x < 5; x++) {
-            System.out.print("x ");
+        //Square
+        /*
+        for (int y = 0; y < 5; y++) {
+            for(int x = 0; x < 5; x++) {
+                System.out.print("x ");
+            }
+            System.out.println(" ");
         }
-         System.out.println(" ");
-    }
-*/
+        */
 
-//  Triangle
-/*
-    for (int y = 0; y < 5; y++) {
-        for(int x = 0; x < y + 1;x++ ) {
+        //Triangle
+        /*
+        for (int y = 0; y < 5; y++) {
+            for(int x = 0; x < y + 1;x++ ) {
+                System.out.print("x ");
+            }
+            System.out.println(" ");
+        }
+        */
+
+        //Triangle the other way
+        /* 
+        for (int y = 0; y < 5; y++) {
+            for(int x = 5; x > y ;x-- ) {
+                System.out.print("  ");
+            }
+            for(int z = 0; z < y + 1;z++ ) {
+                System.out.print("x ");
+            }
+        System.out.println(" ");
+        }
+        */
+
+        //Upside down triangle
+        /*
+        for (int y = 0; y < 5; y++) {
+            for(int x = 5; x > y ;x--) {
+                System.out.print("x ");
+            }
+            System.out.println(" ");
+        }
+        */
+
+        //Equilateral Triangle
+        /*
+        for (int y = 0; y < 5; y++) {
+            for(int x = 5; x > y ;x-- ) {
+                System.out.print(" ");
+            }
+            for(int z = 0; z < y + 1;z++ ) {
+                System.out.print("x ");
+            }
+            System.out.println(" ");
+        }
+        */
+
+        //Diamond
+        /*
+        for (int y = 0; y < 5; y++) {
+            for(int x = 5; x > y ;x-- ) {
+                System.out.print(" ");
+            }
+            for(int z = 0; z < y + 1;z++ ) {
+                System.out.print("x ");
+            }
+            System.out.println(" ");
+        }
+        for (int y = 0; y < 5; y++) {
+            for(int x = 0; x < y + 1;x++ ) {
+                System.out.print(" ");
+            }
+        for(int z = 5; z > y ;z-- ) {
             System.out.print("x ");
         }
         System.out.println(" ");
-    }
-*/
-
-//Triangle the other way
-/* 
-    for (int y = 0; y < 5; y++) {
-        for(int x = 5; x > y ;x-- ) {
-            System.out.print("  ");
         }
-        for(int z = 0; z < y + 1;z++ ) {
+        */
+
+        //Diamond asking you how many blocks you want it
+        /*
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("How big do you want to make the diamond?");
+        int input;
+        input = Scan.nextInt();
+        for (int y = 0; y < input; y++) {
+            for(int x = input; x > y ;x-- ) {
+                System.out.print(" ");
+            }
+            for(int z = 0; z < y + 1;z++ ) {
+                System.out.print("x ");
+            }
+            System.out.println(" ");
+        }
+        for (int y = 0; y < input; y++) {
+            for(int x = 0; x < y + 1;x++ ) {
+                System.out.print(" ");
+            }
+            for(int z = input; z > y ;z-- ) {
+                System.out.print("x ");
+            }
+            System.out.println(" ");
+        }
+        */
+
+        //Hollow Diamond asking how many blocks you want it
+        /*
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("How big do you want to make the diamond?");
+        int input;
+        input = Scan.nextInt();
+        for (int y = 0; y < input; y++) {
+            for(int x = input; x > y ;x-- ) {
+                System.out.print(" ");
+            }
+            for(int z = 0; z < y + 1;z++ ) {
+                if(z == 0 || z == y) {
+                    System.out.print("x ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        for (int y = 0; y < input; y++) {
+            for(int x = 0; x < y + 1;x++ ) {
+                System.out.print(" ");
+            }a
+            for(int z = input; z > y ;z-- ) {
+                if(z == input || z == y + 1) {
+                    System.out.print("x ");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println(" ");
+        }
+        */
+        //t
+        /*
+        for(int i = 0; i < 2; i++) {
+            for(int d = 0; d < 4; d++) {
+                System.out.print(" ");
+            }
+            System.out.println("x");
+        }
+        for(int j = 0; j < 5; j++) {
             System.out.print("x ");
         }
-    System.out.println(" ");
-    }
-*/
-
-//  Upside down triangle
-/*
-    for (int y = 0; y < 5; y++) {
-        for(int x = 5; x > y ;x--) {
-            System.out.print("x ");
+        System.out.println("");
+        for(int i = 0; i < 2; i++) {
+            for(int d = 0; d < 4; d++) {
+                System.out.print(" ");
+            }
+            System.out.println("x");
         }
-        System.out.println(" ");
-    }
-*/
-
-//Equilateral Triangle
-///*
-    for (int y = 0; y < 5; y++) {
-        for(int x = 5; x > y ;x-- ) {
-            System.out.print(" ");
-        }
-        for(int z = 0; z < y + 1;z++ ) {
-            System.out.print("x ");
-        }
-    System.out.println(" ");
-    }
-//*/
-
-//Diamond
-/*
-for (int y = 0; y < 5; y++) {
-    for(int x = 5; x > y ;x-- ) {
-        System.out.print(" ");
-    }
-    for(int z = 0; z < y + 1;z++ ) {
-        System.out.print("x ");
-    }
-    System.out.println(" ");
-}
-for (int y = 0; y < 5; y++) {
-    for(int x = 0; x < y + 1;x++ ) {
-        System.out.print(" ");
-    }
-    for(int z = 5; z > y ;z-- ) {
-        System.out.print("x ");
-    }
-    System.out.println(" ");
-}
-*/
-
-//Diamond asking you how many blocks you want it
-/*
-Scanner Scan = new Scanner(System.in);
-System.out.println("How big do you want to make the diamond?");
-int input;
-input = Scan.nextInt();
-for (int y = 0; y < input; y++) {
-    for(int x = input; x > y ;x-- ) {
-        System.out.print(" ");
-    }
-    for(int z = 0; z < y + 1;z++ ) {
-        System.out.print("x ");
-    }
-    System.out.println(" ");
-}
-for (int y = 0; y < input; y++) {
-    for(int x = 0; x < y + 1;x++ ) {
-        System.out.print(" ");
-    }
-    for(int z = input; z > y ;z-- ) {
-        System.out.print("x ");
-    }
-    System.out.println(" ");
-}
-*/
-
-//Hollow Diamond asking how many blocks you want it
-/*
-Scanner Scan = new Scanner(System.in);
-System.out.println("How big do you want to make the diamond?");
-int input;
-input = Scan.nextInt();
-for (int y = 0; y < input; y++) {
-    for(int x = input; x > y ;x-- ) {
-        System.out.print(" ");
-    }
-    for(int z = 0; z < y + 1;z++ ) {
-        if(z == 0 || z == y) {
-            System.out.print("x ");
-        }
-        else {
-            System.out.print("  ");
-        }
-    }
-    System.out.println(" ");
-}
-for (int y = 0; y < input; y++) {
-    for(int x = 0; x < y + 1;x++ ) {
-        System.out.print(" ");
-    }
-    for(int z = input; z > y ;z-- ) {
-        if(z == input || z == y + 1) {
-            System.out.print("x ");
-        }
-        else {
-            System.out.print("  ");
-        }
-    }
-    System.out.println(" ");
-}
-*/
-
+        */
     }
 }
