9d801170a1869a2c796f9f961f6b07447d47aa6a
DillTheBeast <68816809+DillTheBeast@users.noreply.github.com>
2023-02-20 15:59:18 -0500

    t

---
+++ BankManager.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class BankManager {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the Bank Manager. Please enter what you want to do: \n1: Log in \n 2:Sign up \n3: Exit");
+        int input = scan.nextInt();
+
+        if(input == 1) {
+
+        }
+        else if(input == 2) {
+
+        }
+        else if(input == 3) {
+
+        }
+        else {
+            System.out.println("That is not an option please try again");
+        }
+    }
+}
