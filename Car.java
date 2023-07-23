a8feb34f12be90214507d7a9a8eb419ad1b45f0f
DillTheBeast <dillonmaltese@gmail.com>
2023-07-23 12:37:10 -0400

    cs

--- Car.java
+++ Car.java
@@ -1,16 +1,20 @@
 public class Car {
+    //Defining the variables going to be used
     private String color;
     private int size;
     private int wheels;
 
     public Car(String color, int size, int wheels) {
+        //Turning the variables into an object
         this.color = color;
         this.wheels = wheels;
         this.size = size;
     }
+    //Method takes the variable and gets what is put in for the variable
     public String getColor() {
         return color;
     }
+    //Method sets the object to the variable
     public void setColor(String color) {
         this.color = color;
     }
@@ -26,6 +30,7 @@ public class Car {
     public void setSize(int size) {
         this.size = size;
     }
+    //Method converts all of the variables into a String to be printed
     public String toString() {
         return "Color: " + this.color + ", Size: " + this.size + ", Wheels: " + this.wheels;
     }
