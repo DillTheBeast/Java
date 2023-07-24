1657f2c76a86915e200a5da188eeba5e5c927842
DillTheBeast <dillonmaltese@gmail.com>
2023-07-24 13:49:50 -0400

    asd

--- TEST.java
+++ TEST.java
@@ -2,25 +2,9 @@ import java.util.Scanner;
 
 public class TEST {
     public static void main(String[] args) {
-        //inputs and variables
-        /*
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your age");
-        int age = scan.nextInt();
-        System.out.println("Your age is " + age);
-        */
-        //If, else if, and else statements
-        /*
-        int age = 12;
-        if(age == 12) {
-            System.out.println("You are 12");
-        }
-        else if(age == 13) {
-            System.out.println("You are 13");
-        }
-        else {
-            System.out.println("You are not 12 or 13");
-        }
-        */
+        int n = 0;
+
+        String strNum = String.valueOf(n);
+        System.out.println(strNum);
     }
 }
