0c1cffd561b7c56133b1919e2898860b27a815d6
DillTheBeast <dillonmaltese@gmail.com>
2023-07-26 20:58:59 -0400

    asd

--- PostFixCalc.java
+++ PostFixCalc.java
@@ -7,11 +7,48 @@ public class PostFixCalc {
         boolean done = false;
         System.out.println("Please enter the post fix equation");
         equationString = Scan.nextLine();
-        String[] equationArray =equationString.split(" ");
+        String[] equationArray = equationString.split(" ");
+        int numCount = 0;
+        Stack stack = new Stack(2);
+        int num1;
+        int num2;
+        int ans = 0;
 
-        while(done == false) {
-            
+        for(int i = 0; i < equationArray.length; i++) {
+            try {
+                if(equationArray[i].equals("+")) {
+                num2 = stack.pop();
+                num1 = stack.pop();
+                ans = num1 + num2;
+                }
+                else if(equationArray[i].equals("-")) {
+                    num2 = stack.pop();
+                    num1 = stack.pop();
+                    ans = num1 - num2;
+                }
+                else if(equationArray[i].equals("*")) {
+                    num2 = stack.pop();
+                    num1 = stack.pop();
+                    ans = num1 * num2;
+                }
+                else if(equationArray[i].equals("/")) {
+                    num2 = stack.pop();
+                    num1 = stack.pop();
+                    ans = num1 / num2;
+                }
+                else {
+                    try {
+                        stack.push(Integer.valueOf(equationArray[i]));
+                    } catch (Exception e) {
+                        throw new IllegalStateException("This is not a number or an operation");
+                    }
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("Invalid Notation");
+                break;
+            }
         }
-
+        System.out.println(ans);
     }
 }
