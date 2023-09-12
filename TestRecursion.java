8a8f7e60d8875081303b63372c87f60873593f06
DillTheBeast <dillonmaltese@gmail.com>
2023-09-12 09:35:05 -0400

    Update TestRecursion.java

--- TestRecursion.java
+++ TestRecursion.java
@@ -1,13 +1,6 @@
 public class TestRecursion {
     public static void main(String[] args) {
-        int n = 0;
-        Test(n);
-    }
-
-    public static void Test(int n) {
-        if(n < 5){
-            System.out.println("Hello");
-            Test(n+1);
-        }
+        String n = "This";
+        System.out.println(n);
     }
 }
