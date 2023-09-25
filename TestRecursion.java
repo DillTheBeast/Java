ea80fbce269fb57f3cecdab473353d524b3a4c15
DillTheBeast <dillonmaltese@gmail.com>
2023-09-25 11:28:59 -0400

    Update TestRecursion.java

--- TestRecursion.java
+++ TestRecursion.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class TestRecursion {
     public static void main(String[] args) {
-        String n = "This";
-        System.out.println(n);
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if(n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
