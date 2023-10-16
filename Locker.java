bb689f62566ead9f2ca645041dc22a8cb956cd47
DillTheBeast <dillonmaltese@gmail.com>
2023-10-16 18:31:49 -0400

    Create Locker.java

---
+++ Locker.java
@@ -0,0 +1,11 @@
+public class Locker {
+    public static void main(String[] args) {
+        math(0);
+    }
+    public static Integer math(int[] open, int count, int iterations) {
+        if(open - open < 1) {
+            return open;
+        }
+
+    }
+}
