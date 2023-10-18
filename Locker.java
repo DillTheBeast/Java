550251e5e2f9e360a831abfd61039f562f321122
DillTheBeast <dillonmaltese@gmail.com>
2023-10-18 11:07:48 -0400

    asd

--- Locker.java
+++ Locker.java
@@ -1,6 +1,7 @@
 public class Locker {
     public static void main(String[] args) {
-        math(0);
+        
+        math();
     }
     public static Integer math(int[] open, int count, int iterations) {
         if(open - open < 1) {
--- SigFigCalc.java
+++ SigFigCalc.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
+import java.math.BigDecimal;
+
 public class SigFigCalc {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean valid = false;
-        double start = 0.0;
+        BigDecimal start = BigDecimal.ZERO;
         int round = 0;
 
         System.out.println("Would you like to do Sig Fig or Sci Not \nPress 1 or 2");
         int choice = scan.nextInt();
         if(choice == 1) {
-            while(valid) {
+            while(!valid) {
                 System.out.println("Enter the number");
-                start = scan.nextDouble();
+                start = scan.nextBigDecimal();
                 System.out.println("How many digits would you like to round it to");
                 round = scan.nextInt();
                 if(String.valueOf(start).length() >= round)
@@ -20,24 +22,63 @@ public class SigFigCalc {
                     System.out.println("Not possible please redo");
 
             }
-            double end = SigFig(start, round);
+            String end = SigFig(start, round);
+            System.out.println(end);
         } else if(choice == 2) {
-
+            System.out.println("Enter the number");
+            start = scan.nextBigDecimal();
+            String end = SciNot(start);
+            System.out.println(end);
         }
     }
-    public static double SigFig(double num, int round) {
-        //Need to find the amount of sig figs in the problem. 
+    public static String SigFig(BigDecimal num, int round) {
         String numString = String.valueOf(num);
+        String finalNum = "";
+        int sizeCount = 0;
         boolean first = true;
-        for(int i = 0; i < num; i++) {
-            if((numString.charAt(i) != '0' || numString.charAt(i) != '.') && first == true) {
+        
+        for(int i = 0; i < numString.length(); i++) {
+            char currentChar = numString.charAt(i);
 
-            } else if(numString.charAt(i) == '.' && first == true) {
-
-            }
-            else {
+            if((currentChar == '0' || currentChar == '.') && first) {
+                finalNum += currentChar;
+            } else if(currentChar == '.') {
+                finalNum += currentChar;
+            } else {
+                if (sizeCount == round - 1) {
+                    if (i < numString.length() - 1 && Character.getNumericValue(numString.charAt(i + 1)) >= 5) {
+                        int place = Character.getNumericValue(currentChar);
+                        place++;
+                        finalNum += String.valueOf(place);
+                    } else {
+                        finalNum += currentChar;
+                    }
+                    return finalNum;
+                }
                 first = false;
+                finalNum += currentChar;
+                sizeCount++;
             }
         }
+        return finalNum;
     }
+    public static String SciNot(BigDecimal num) {
+        String numString = String.valueOf(num);
+        String finalNum = "";
+        int sizeCount = 0;
+        if(numString.charAt(0) == '0') {
+            //Need to be negative power
+            if(numString.charAt(1) == '.') {
+                for(int i = 2; i < numString.length(); i++) {
+                    if(numString.charAt(i) != 0)
+                        sizeCount++;
+            }
+            }
+        } else if(numString.charAt(0) == '.') {
+            //Need to be negative power
+        } else {
+            //Need to be positive power
+        }
+    }
+
 }
