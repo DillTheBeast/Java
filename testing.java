b5c3e2f1f78a5400a246eeb770b4c0bea0f1912c
DillTheBeast <dillonmaltese@gmail.com>
2023-11-20 23:44:21 -0500

    Create testing.java

---
+++ testing.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+
+public class testing {
+    public static void main(String[] args) {
+        String str1 = "listen123";
+        String str2 = "silent231";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+
+    // Function to check if two strings are anagrams
+    private static boolean areAnagrams(String str1, String str2) {
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
